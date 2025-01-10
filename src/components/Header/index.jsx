ee42df4fb02d3e1b57abe41ebca753559fa743f4
Zhm1603 <zhanel.marat2006@gmail.com>
2025-01-11 04:24:05 +0600

    header layout fixing

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,31 +1,67 @@
-import React from "react";
+import React, { useEffect, useState }from "react";
 import { useTelegram } from "../../context/TelegramProvider";
-import level from "../../assets/level.svg"
-import wallet from "../../assets/wallet.svg"
-import profile from "../../assets/user.svg"
+import levelIcon from "../../assets/level.svg"
+import walletIcon from "../../assets/wallet.svg"
+import profileIcon from "../../assets/user.svg"
 import "./style.css";
 
 const Header = () => {
     const { user } = useTelegram();
+    const [level, setLevel] = useState(5);
+    const [balance, setBalance] = useState(124124);
 
-    return (
-        <div className="header">
+    const userInfoData = [
+        {
+            className:"header-level",
+            value: level,
+            action: setLevel,
+            iconPath: levelIcon,
+        },
+        {
+            className: "header-wallet",
+            value: balance,
+            action: setBalance,
+            iconPath: walletIcon,
+        },
+    ];
+    
+    //useEffect(())
+
+    const Profile = () =>{
+        return(
             <div className="left-side">
-                <img src = {profile} alt = "user-img" className="header-icon-container" />
+                <img 
+                    src = {profileIcon} 
+                    alt = "user-img" 
+                    className="header-icon-container" />
                 <div className="header-username-container">
                     @{user?.username || "username"}
                 </div>
             </div>
+        );
+    };
+
+    const ProfileInfo = () =>{
+        return(
             <div className="right-side">
-                <div className="header-level-container">
-                    <img src = {level} alt = "level-img" className="header-icon-container" />
-                    <div className="header-level">5</div>
-                </div>
-                <div className="header-wallet-container">
-                    <img src = {wallet} alt ="wallet-img" className="header-icon-container" /> 
-                    <div className="header-wallet">124 124</div>
-                </div>
+                {userInfoData.map((item) =>(
+                    <div className="header-data-container">
+                        <img
+                            src={item.iconPath}
+                            alt = "img"
+                            className={item.className}
+                        />
+                        <div className="header-level">{item.value}</div>
+                    </div>
+            ))}
             </div>
+        );
+    };
+
+    return (
+        <div className="header">
+            <Profile />
+            <ProfileInfo />
         </div>
     );
 }
